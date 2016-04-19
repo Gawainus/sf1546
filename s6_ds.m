937273b71d788b9b4a3260fa4898c37923f2e6e2
Dan S <ds136@outlook.com>
2016-04-19 21:41:01 +0200

    s6_ds.m updated

--- s6_ds.m
+++ s6_ds.m
@@ -1,5 +1,12 @@
+N=25
+u = ones(N*2,1)
+u_old = zeros(N*2,1)
+test = [1]
+while abs(u(end)-u_old(end)) > 0.5
+u_old = u
+test = test + [abs(u(end)-u_old(end))] 
 % number of sections
-N=25;
+N=N*2;
 K=1; %3.156
 
 u_0=100;
@@ -12,8 +19,7 @@ k = 20
 
 h=(r_end-r_start)/N
 
-%Reached by solving the linear problem with u_0 = 100 and Te = 300
-startguess_u = [107 113 119 125 131 136 141 147 151 156 161 166 170 175 179 183 187 191 195 199 202 206 209 213 216]'
+startguess_u = 100:200/(N-1):300
 
 % from r0 to rN
 r_raw=(1:h:2);
@@ -45,34 +51,34 @@ corr = ones(N,1)
 %F = A
 %J = A  %The jacobian only differs from A in the last row.
 
-u = startguess_u
+u = startguess_u'
 
 itercheck = 1
 dominance_radiation = -1
 
 Te = 200
 
-corr = 1
+
 
 while dominance_radiation <= 0
   Te = Te + 100
   supplement = alpha*Te - deltabetaepsilon*u(end)^4 + deltabetaepsilon*Te^4 + (2*h*r(end)*alpha*Te)/(k*h^2) - (2*h*r(end)*deltabetaepsilon*u(end)^4)/(k*h^2) + (2*h*r(end)*deltabetaepsilon*Te^4)/(k*h^2);
   supplement_jaco = -4*deltabetaepsilon*u(end)^3 - (8*h*r(end)*deltabetaepsilon*u(end)^3)/(k*h^2);
-  itercheck = 0
- while norm(corr, Inf) > 1e-10
-    F = A*u; F(1) = F(1) + (r(1)/h^2 - 1/(2*h))*u_0; F(end) = F(end) + supplement
-    J = A; J(end) = J(end) + supplement_jaco
-    corr = J\F
-    u = u-corr
-    itercheck = itercheck + 1
+  corr = 1
+ while norm(corr, Inf) > 1e-9
+    F = A*u; F(1) = F(1) + (r(1)/h^2 - 1/(2*h))*u_0; F(end) = F(end) + supplement;
+    J = A; J(end) = J(end) + supplement_jaco;
+    corr = J\F;
+    u = u-corr;
   end
   dominance_radiation = abs(deltabetaepsilon*(u(end)^4 - Te^4)) - abs(alpha*(u(end) - Te));
 end
-
+end
 figure
 plot(r,u)
 title('Plot of Temperature with respect to Radius, K=1')
 xlabel('radius, cm')
 ylabel('temperature, Celsius')
+
 
 
