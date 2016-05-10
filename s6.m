6a3239018ea63cb3d2fb87e6609426e5f5805c97
Dan S <ds136@outlook.com>
2016-05-10 19:11:49 +0200

    at last

--- s6.m
+++ s6.m
@@ -12,7 +12,7 @@ u = ones(N*2,1); u(end)=1000;
 u_old = zeros(N*2,1);
 u_oldest = u_old;
 
-while abs(u(end)-u_old(end)) > 0.5*1e-3
+while abs(u(end)-u_old(end)) > 0.5 * 1e-3
 differencecheck = (u(end)-u_old(end))/(u_old(end)-u_oldest(end))
 u_oldest = u_old;
 u_old = u;
@@ -21,7 +21,7 @@ N=N*2;
 
 h=(r_end-r_start)/N;
 
-startguess_u = 100:200/(N-1):300;
+startguess_u = 100:1100/(N-1):1200;
 
 % from r0 to rN
 r_raw=(1:h:2);
@@ -30,16 +30,19 @@ r_raw=(1:h:2);
 r=r_raw(2:end);
 
 % from r1 to rN-1 this is for diag plus 1
-%r_p1=r(1:end-1);
-r_p1=r(1:end);
+r_p1=r(1:end-1);
+%r_p1=r(1:end);
 
-% from r2 to rN-1 this is from diag minus 1
-%r_m1=r(2:end);
-r_m1=r(1:end);
+% from r2 to rN this is from diag minus 1
+r_m1=r(2:end);
+%r_m1=r(1:end);
 
 v=factor2(r,h);
 v_p1=factor3(r_p1,h);
 v_m1=factor1(r_m1,h);
+
+v_p1 = [1 v_p1];   %last row placeholder
+v_m1 = [v_m1 1];
 
 %D=diag(v);
 %D_p1=diag(v_p1,1);
@@ -59,10 +62,9 @@ u = startguess_u';
 itercheck = 1;
 dominance_radiation = -1;
 
-%Te = 200;
-%Te = 1200
-%while dominance_radiation <= 0
-%  Te = Te + 100;
+Te = 200;
+while dominance_radiation <= 0
+  Te = Te + 100;
   supplement = alpha*Te - deltabetaepsilon*u(end)^4 + deltabetaepsilon*Te^4 + (2*h*r(end)*alpha*Te)/(k*h^2) - (2*h*r(end)*deltabetaepsilon*u(end)^4)/(k*h^2) + (2*h*r(end)*deltabetaepsilon*Te^4)/(k*h^2);
   supplement_jaco = -4*deltabetaepsilon*u(end)^3 - (8*h*r(end)*deltabetaepsilon*u(end)^3)/(k*h^2);
   corr = 1;
@@ -74,12 +76,9 @@ dominance_radiation = -1;
   end
   dominance_radiation = abs(deltabetaepsilon*(u(end)^4 - Te^4)) - abs(alpha*(u(end) - Te));
 end
-%end
+end
 figure
 plot(r,u)
 title('Plot of Temperature with respect to Radius, Te=1200')
 xlabel('radius, cm')
 ylabel('temperature, Celsius')
-
-
-
